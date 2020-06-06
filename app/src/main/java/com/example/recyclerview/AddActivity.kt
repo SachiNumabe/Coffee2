bd76656f8fc77f5235bd6df629c1b3f82d0fb843
numabesachi <numabesachi@gmail.com>
2020-06-06 18:17:02 +0900

    torimaugoku3

--- app/src/main/java/com/example/recyclerview/AddActivity.kt
+++ app/src/main/java/com/example/recyclerview/AddActivity.kt
@@ -1,13 +1,59 @@
 package com.example.recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add.*
+import java.io.FileDescriptor
+import java.io.IOException
+
 
 class AddActivity : AppCompatActivity() {
+
+    private val RESULT_PICK_IMAGEFILE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
 
+
+        imageViewAdd.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "image/*"
+            startActivityForResult(intent, RESULT_PICK_IMAGEFILE)
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == Activity.RESULT_OK) {
+            var uri: Uri? = null
+            if (resultData != null) {
+                uri = resultData.data
+                if(uri != null){
+                    try {
+                    val bmp = getBitmapFromUri(uri)
+                    imageViewAdd.setImageBitmap(bmp)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun getBitmapFromUri(uri: Uri): Bitmap {
+        val parcelFileDescriptor =
+            contentResolver.openFileDescriptor(uri, "r")
+        val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+        val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+        parcelFileDescriptor.close()
+        return image
     }
 }
